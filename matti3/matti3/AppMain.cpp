c1d4a688c41626ba52415e63eacc03745b584168
Tomimura-yoshinori3225 <138739518+Tomimura-yoshinori3225@users.noreply.github.com>
2023-07-13 15:01:09 +0900

    APPMain.cppとFreamControl.cppの追加
    APPMain.cppとFreamControl.cppを書いた

--- matti3/matti3/AppMain.cpp
+++ matti3/matti3/AppMain.cpp
@@ -47,4 +47,68 @@
 *プログラムの開始
 *******************************/
 
-int WINAPI WinMai
+int WINAPI WinMain(_In_HINSTANCE hinsutance, _IN_opt_HINSTANCE hPrevINsance,
+	_IN_LPSTR lpCmdLine, _IN_intnShowCmd)
+{
+	//ウィンドウスタイル設定
+	setMainWindowTest("Match 3 Puzzle");
+	//ウィンドウモードで起動
+	ChangeWindowMode(TRUE);
+	//画面サイズの最大サイズ、カラービット数を設定
+	setGraphMode(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_COLORBIT);
+
+	//DXライブラリ初期化処理
+
+	//エラーが発生したら、終了する
+	if (DxLib_Init() == D_ERROR)
+	{
+		return D_ERROR;
+	}
+
+	//各種機能の初期化処理
+	FreamControl_Initialize();    //フレームレート制御機能
+	Input_Initialize();           //入力制御機能
+
+	//シーンマネージャー初期化処理
+
+	//エラーが発生したら、終了する
+	if (SceneManager_Initialize(E_TITLE) == D_ERROR)
+	{
+		return D_ERROR;
+	}
+
+	//描画先画面を裏にする
+	setDrawScreen(DX_SCREEN_BACK);
+
+	//文字サイズを設定
+	setFontSize(FONT_SIZE);
+
+	//ゲームループ
+	while (ProcessMessage() != D_ERROR && Input_Escape() == FALSE)
+	{
+
+		//入力制御機能更新処理
+		Input_Update();
+
+		//シーンマネージャー更新処理
+		SceneManager_Update();
+
+		//画面クリア
+		ClearDrawScreen();
+
+		//シーンマネージャー描画処理
+		SceneManager_Draw();
+
+		//フレームレート制御処理
+		FreaControl_Update();
+
+		//画面の内容を表画面に反映
+		ScreenFlip();
+
+	}
+
+	//Dxライブラリ使用の終了処理
+	DxLib_End();
+
+	return 0;
+}
