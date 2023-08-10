ae47a3d7c9d82e00493e8feae700e4fe8dc2676e
Tomimura-yoshinori3225 <138739518+Tomimura-yoshinori3225@users.noreply.github.com>
2023-08-10 10:54:17 +0900

    InputControl.hを追加
    InputControl.hを追加した

---
+++ matti3/matti3/InputControl.h
@@ -0,0 +1,32 @@
+#pragma once
+
+/**************************
+
+*マクロ定義
+
+**************************/
+
+
+/*************************
+
+*型定義
+
+************************/
+
+
+/*******************************
+
+*プロトタイプ宣言
+
+********************************/
+
+void Input_Initialize(void);
+void Input_Update(void);
+int Input_Escape(void);
+
+int GetOldKey(int Key);
+int GetNowKey(int key);
+int GetKeyFlg(int Key);
+int GetMousePositionx(void);
+int GetMousePositiony(void);
+
