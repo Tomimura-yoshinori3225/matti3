4bcd606df26e378e5f731bfc08b4a7652f193018
Tomimura-yoshinori3225 <138739518+Tomimura-yoshinori3225@users.noreply.github.com>
2023-08-16 12:08:52 +0900

    Stageを書いている
    Stageを書いている途中

--- matti3/matti3/Stage.cpp
+++ matti3/matti3/Stage.cpp
@@ -66,3 +66,106 @@ int ClearFlag;
 
 int BlockImage[BLOCK_IMAGE_MAX];   //ブロック画像
 int StageImage;                     //背景用画像
+
+int ClickSE;                        //クリックSE
+int FadeOutSE;                     //フェードアウトSE
+int MoveBlockSE;                   //ブロック移動SE
+
+/*******************************
+
+*プロトタイプ宣言
+
+******************************/
+
+int combo_check(int x, int y);
+void combo_check_h(int y, int x, int* cnt, int* col);
+void combo_check_w(int y, int x, int* cnt, int* col);
+void save_block(void);
+
+void restore_block(void);
+
+/*******************************
+
+*ステージ制御機能：初期化処理
+
+*引数：なし
+
+*戻り値：エラー情報
+
+*******************************/
+
+int StageInitialize(void)
+{
+	int ret = 0;
+	int i;
+
+	//画像読み込み
+	LoadDivGraph("images/block.png", BLOCK_IMAGE_MAX, BLOCK_IMAGE_MAX, 1,
+		BLOCKSIZE, BLOCKSIZE, BlockImage);
+	StageImage = LoadGraph("images/stage.png");
+
+
+	//音源読み込み
+	ClickSE = LoadSoundMem("sounds/click_se.mp3");
+	FadeOutSE = LoadSoundMem("sounds/fadeout_se.mp3");
+	MoveBlockSE = LoadSoundMem("sounds/moveblok_se.mp3");
+
+	//ブロック生成処理
+	CreateBlock();
+
+	ClickStatus = E_NONE;
+	Stage_State = 0;
+	Stage_Score = 0;
+	ClearFlag = FALSE;
+
+	for (i = 0; i < 3; i++)
+	{
+		Select[i].x = 0;
+		Select[i].y = 0;
+	}
+
+	//エラーチェック
+	for (i = 0; i < BLOCK_IMAGE_MAX; i++)
+	{
+		if (BlockImage[i] == -1)
+		{
+			ret = -1;
+			break;
+		}
+	}
+
+	if (StageImage == -1)
+	{
+		ret = -1;
+	}
+
+	if (ClickSE == -1)
+	{
+		ret = -1;
+	}
+
+	if (FadeOutSE == -1)
+	{
+		ret = -1;
+	}
+
+	if (MoveBlockSE == -1)
+	{
+		ret = -1;
+	}
+
+	return ret;
+}
+
+
+
+
+/*****************************
+
+*ステージ制限機能：ステージの描画
+
+*引数：なし
+
+*戻り値：なし
+
+********************************/
