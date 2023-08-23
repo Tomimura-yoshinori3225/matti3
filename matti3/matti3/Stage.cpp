3ef1ada3a7522033a637be84dcecd471e039bcda
Tomimura-yoshinori3225 <138739518+Tomimura-yoshinori3225@users.noreply.github.com>
2023-08-23 22:20:10 +0900

    Stageを書いている途中
    Stageを書いている途中

--- matti3/matti3/Stage.cpp
+++ matti3/matti3/Stage.cpp
@@ -514,3 +514,164 @@ void CheckBlock(void)
 		//連鎖が３つ以上ならブロックを消しブロック移動処理へ移行する
 		Stage_State = 1;
 	}
+
+
+	/***************************
+	
+	*ステージ制御機能：クリア条件チェック処理
+	
+	*引数：なし
+	
+
+	*戻り値：なし
+	
+	*備考：クリア条件フラグを０とし、各スクールの削除ブロックが
+	レベルよりも数が少なかったらチェック処理を中断してゲームを続行する。
+	**********************************/
+
+	void CheckClear(void)
+	{
+		int i;
+
+		for (i = 0; i < ITEM_MAX; i++)
+		{
+			if (Item[i] >= Stage_Mission)
+			{
+				ClearFlag = TRUE;
+				break;
+			}
+		}
+		if (ClearFlag != TRUE)
+		{
+			Stage_State = 0;
+		}
+	}
+
+	/*****************************
+	
+	*ステージ制御機能：ステージステータス情報取得処理
+	
+	*引数：なし
+	
+	*戻り値：ステージのステータス情報
+	*********************************/
+
+	int Get_StageState(void)
+	{
+		return Stage_State;
+	}
+
+	/***************************
+	
+	*ステージ制御機能：ミッション情報取得処理
+	
+	*引数：なし
+	
+	*戻り値：ミッションがクリアしているか
+	*********************************/
+
+	int Get_StageClearFlag(void)
+	{
+		return ClearFlag;
+	}
+
+	/****************************
+	
+	*ステージ制御機能：ミッション情報処理
+	
+	*引数：なし
+	
+	*戻り値：ミッションがクリアしているか
+	*******************************/
+
+	int Get_StageScore(void)
+	{
+		return Stage_Score;
+	}
+
+	/********************************
+	
+	*ステージ制御機能：ミッション情報取得処理
+	
+	*引数：なし
+	
+	*戻り値：ミッションがクリアしているか
+	***************************/
+
+	void Set_StageMisson(int mission)
+	{
+		Stage_Mission += mission;
+	}
+
+	/****************************
+	
+	*ステージ制御機能：連鎖チェック処理
+	
+	*引数１：ブロックYマス
+	
+	*引数２：ブロックXマス
+	
+	*戻り値：連鎖無(0：なし　１：有り）
+	
+	*********************************/
+
+	int combo_check(int y, int x)
+	{
+		int ret = FALSE;
+
+		//縦方向のチェック
+		int CountH = 0;
+		int ColorH = 0;
+
+		save_block();
+		combo_check_h(y, x, &CountH, &ColorH);
+		if (CountH < 3)
+		{
+			restore_block();         //3個未満なら戻す
+		}
+
+		//横方向のチェック
+		int CountW = 0;
+		int ColorW = 0;
+		save_block();
+		combo_check_w(y, x, &CountW, &ColorW);
+		if (CountW < 3)
+		{
+			restore_block();
+		}
+
+
+
+		//3つ以上で並んでいるか？
+		if ((CountaH >= 3 || CountW >= 3))
+		{
+			if (CountH >= 3)
+			{
+				Item[ColorH - 1] += CountH;
+				Stage_Score += CountH * 10;
+			}
+			if (CountW >= 3)
+			{
+				Item[ColorH - 1] += CountH;
+				Stage_Score += CountW * 10;
+			}
+			ret = TRUE;
+		}
+		return ret;
+	}
+
+	/*****************************
+	
+	*ステージ制御機能：連鎖チェック処理(縦方向）
+	
+	*引数：なし
+	
+	*戻り値：連鎖有無(0:無し　１：有り）
+	****************************/
+
+	void combo_check_h(int y, int x, int* cnt, int* col)
+	{
+		int Color = 0;
+		//対象のブロックが外枠の場合はreturnで処理を抜ける
+	}
+
