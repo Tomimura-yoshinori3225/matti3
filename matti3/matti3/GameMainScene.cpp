c94f8ba087ad6f10ad26eb78025a2b1a44acb8da
Tomimura-yoshinori3225 <138739518+Tomimura-yoshinori3225@users.noreply.github.com>
2023-08-31 10:14:18 +0900

    完成？
    完成？

--- matti3/matti3/GameMainScene.cpp
+++ matti3/matti3/GameMainScene.cpp
@@ -76,17 +76,28 @@ int GameMainScene_Initialize(void)
 	}
 
 	//ゲームプレイが初回かどうか？
+
 	if (GameCount == 0)
 	{
 		GameScore = 0;     //スコアの初期化
 		GameLevel = 1;     //ゲームレベルの初期化
 		Set_StageMission(3);  //ミッションの初期化
 		GameCount++;       //次回の設定
+		
 	}
+
 	else
 	{
-		GameLevel++;     //ゲームレベルの更新
-		Set_StageMission(3);  //ミッションを増やす
+		GameCount++;
+		if (GameCount%2 ==1)
+		{
+			GameLevel++;//ゲームレベルの更新
+			Set_StageMission(3);  //ミッションを増やす
+		}
+		
+		
+		
+
 	}
 	GameTime = TIMELIMIT;    //制限時間の初期化
 
